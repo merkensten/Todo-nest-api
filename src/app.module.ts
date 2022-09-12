4bac75e75bd0392cd75e694fd3403e8751714ace
Mattis <m.erkensten@icloud.com>
2022-09-13 00:53:22 +0200

    Skapat struktur för endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    UserModule,
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
