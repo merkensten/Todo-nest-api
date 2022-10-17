bac88bb1849279083aef2f2f5f99f4dc145c740a
Mattis <m.erkensten@icloud.com>
2022-10-17 15:52:20 +0200

    mindre fixes utförda

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,41 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, Put, Delete } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { UserDetails } from './interface/user-details.interface';
 import { UserService } from './user.service';
 
-// Om Jag vill, utveckla denna så att en admin kan hantera användare
+// Endpoints
+// Get: all users
+// Get: user by id
+// Post: create user
+// Put: update user
+// Delete: delete user
+
+// Skapa en guard för att kolla att admin har userLevel 1
+
+// Fixa sedan types för allt
 
 @Controller('/user')
+@UseGuards(JwtGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(JwtGuard)
   @Get()
-  getHello(): string {
+  getAllUsers(): Promise<UserDetails> | string {
     return 'Hello';
   }
 
-  @UseGuards(JwtGuard)
   @Get(':id')
   getUser(@Param('id') id: string): Promise<UserDetails | null> {
     return this.userService.findById(id);
+  }
+
+  @Put(':id')
+  getPut(@Param('id') id: string): any {
+    return 'Hello' + id;
+  }
+
+  @Delete(':id')
+  getDelete(@Param('id') id: string): any {
+    return 'Hello' + id;
   }
 }
